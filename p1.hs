f935105daaa46f37fe26400845bad6e1b23345f6
Daniel Cartwright <chessai@users.noreply.github.com>
2017-08-04 13:18:11 -0600

    Update p1.hs

--- p1.hs
+++ p1.hs
@@ -1,13 +1,18 @@
+import Data.List.Extra (nubOrd)
+
 problem :: String
 problem = "If we list all the natural numbers below 10 that are multiples of 3 or 5, " ++
           "we get 3, 5, 6, and 9. The sum of these multiples is 23. " ++
           "Find the sum of all the multiples of 3 or 5 below 1000"
 
 solve :: String
-solve = show $ solveProblem sum' [3, 5] (1000 - 1)
+solve = show $ solveProblem sumOfMultiples [3, 5] (1000 - 1)
 
 solveProblem :: (Int -> Int -> Int) -> [Int] -> Int -> Int
-solveProblem f ls bound = (foldl (+) 0 $ map (f bound) ls) - f bound (product ls)
+solveProblem f ls bound = g ls - g duplicates
+  where
+    g ls'      = sum $ map (f bound) ls'
+    duplicates = nubOrd [x*y | x <- ls, y <- ls, x /= y]
 
-sum' :: Int -> Int -> Int
-sum' x d = d * (x `div` d) * (1 + x `div` d) `div` 2
+sumOfMultiples :: Int -> Int -> Int
+sumOfMultiples x d = d * (x `div` d) * (1 + x `div` d) `div` 2
